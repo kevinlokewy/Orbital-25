bad27d64adc3367a4b40dabcda85eae396f24edf
brien <brienlim21@gmail.com>
2025-06-02 03:52:57 +0800

    Added login button

--- Orbital-25/frontend/nextjs-frontend/twonoobs/src/app/page.tsx
+++ Orbital-25/frontend/nextjs-frontend/twonoobs/src/app/page.tsx
@@ -7,6 +7,16 @@ export default function HomePage() {
 
   return (
     <main className="p-6 bg-gray-100 min-h-screen">
+      {/*login button*/}
+      <div className='flex justify-end mb-4'>
+        <button
+          onClick={() => window.location.href = '/login'}
+          className='bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600'
+        >
+          Log In
+        </button>
+      </div>
+
       <h1 className="text-3xl font-bold text-center mb-6 text-black">Racetrack Home</h1>
 
       <div className="flex justify-center gap-4 mb-6">
